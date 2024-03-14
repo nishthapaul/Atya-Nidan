51a1e72ccdbbe218ee8900eaea8d137e1c5f7702
Nishtha Paul <nishthapaul@Nishthas-MacBook-Air.local>
2024-03-14 20:35:57 +0530

    Remove unnecessary access modifier

--- health-hub/src/main/java/com/atyanidan/healthhub/dao/UserRepository.java
+++ health-hub/src/main/java/com/atyanidan/healthhub/dao/UserRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
-    public User findByPhoneNumber(String phoneNumber);
+    User findByPhoneNumber(String phoneNumber);
 }
