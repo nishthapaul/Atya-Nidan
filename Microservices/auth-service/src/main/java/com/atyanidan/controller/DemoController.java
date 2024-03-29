528c315061f9921fd825a1d451ee33068a40480f
Nishtha Paul <nishthapaul@Nishthas-MacBook-Air.local>
2024-03-29 12:37:07 +0530

    Set up API Gateway in auth-service

--- Microservices/auth-service/src/main/java/com/atyanidan/controller/DemoController.java
+++ Microservices/auth-service/src/main/java/com/atyanidan/controller/DemoController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/atyanidan/auth/api")
 public class DemoController {
 
     @GetMapping("/demo")
