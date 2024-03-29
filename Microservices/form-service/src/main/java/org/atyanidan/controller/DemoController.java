c860695a9acbcacdd78138d72807d61ba04afa88
Nishtha Paul <nishthapaul@Nishthas-MacBook-Air.local>
2024-03-29 09:59:47 +0530

    Add base path for form service

---
+++ Microservices/form-service/src/main/java/org/atyanidan/controller/DemoController.java
@@ -0,0 +1,16 @@
+package org.atyanidan.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class DemoController {
+
+    @GetMapping("/demo")
+    public String demoMethod() {
+        return "Demo-String";
+    }
+
+}
