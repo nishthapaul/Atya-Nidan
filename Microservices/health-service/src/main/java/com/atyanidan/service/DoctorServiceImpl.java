f37021ba4725c9ca85d716da7420d1b40a0610ef
Nishtha Paul <nishthapaul@Nishthas-MacBook-Air.local>
2024-04-03 20:48:43 +0530

    Add specific error message on post api

--- Microservices/health-service/src/main/java/com/atyanidan/service/DoctorServiceImpl.java
+++ Microservices/health-service/src/main/java/com/atyanidan/service/DoctorServiceImpl.java
@@ -43,8 +43,12 @@ public class DoctorServiceImpl implements DoctorService {
         try {
             return doctorRepository.save(doctor);
         } catch (DataIntegrityViolationException e) {
-            if ( e.getMessage().contains("Duplicate entry") ) {
-                String errorMessage = "Phone number or email already exists.";
+            String exceptionRootCause = e.getRootCause().getMessage();
+            if ( exceptionRootCause.contains("Duplicate entry") && exceptionRootCause.contains("phone_number") ) {
+                String errorMessage = "Phone number already exists.";
+                throw new ConflictException(errorMessage, e);
+            } else if ( exceptionRootCause.contains("Duplicate entry") && exceptionRootCause.contains("email") ) {
+                String errorMessage = "Email ID already exists.";
                 throw new ConflictException(errorMessage, e);
             } else {
                 throw new BadRequestException("Some fields are required and not provided.", e);
