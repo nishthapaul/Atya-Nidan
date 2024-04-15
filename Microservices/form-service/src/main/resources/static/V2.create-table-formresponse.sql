f0303a44e2dd1787788d9dd59d9ba66675f34598
Nishtha Paul <nishthapaul@Nishthas-MacBook-Air.local>
2024-04-15 19:29:24 +0530

    Add create table query for form response table

---
+++ Microservices/form-service/src/main/resources/static/V2.create-table-formresponse.sql
@@ -0,0 +1,18 @@
+CREATE TABLE IF NOT EXISTS Form_Response (
+    form_response_id int AUTO_INCREMENT,
+    form_id int NOT NULL,
+    fieldworker_id int NOT NULL,
+--    patient_id int NOT NULL,
+    submitted_on DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    olap_form_id varchar(50) NOT NULL UNIQUE,
+    primary key (form_response_id)
+);
+
+ALTER TABLE Form_Response
+ADD FOREIGN KEY (form_id) REFERENCES Form(form_id);
+
+ALTER TABLE Form_Response
+ADD FOREIGN KEY (fieldworker_id) REFERENCES Fieldworker(field_worker_id);
+--
+--ALTER TABLE Form_Response
+--ADD FOREIGN KEY (patient_id) REFERENCES Patient(patient_id);
