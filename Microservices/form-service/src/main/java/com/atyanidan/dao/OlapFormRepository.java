f692d4d015ae9f8204c60abaa63a407bf8d894d0
Nishtha Paul <nishthapaul@Nishthas-MacBook-Air.local>
2024-04-18 01:25:07 +0530

    Remove unused imports

--- Microservices/form-service/src/main/java/com/atyanidan/dao/OlapFormRepository.java
+++ Microservices/form-service/src/main/java/com/atyanidan/dao/OlapFormRepository.java
@@ -1,6 +1,5 @@
 package com.atyanidan.dao;
 
-import com.atyanidan.entity.elasticsearch.FormDefinition;
 import com.atyanidan.entity.elasticsearch.OlapForm;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
