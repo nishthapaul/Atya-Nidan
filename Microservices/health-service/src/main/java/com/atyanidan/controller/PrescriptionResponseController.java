b0c6fc04bf284bd9c6a0dc66c02e1a2c7c13b1cf
Nishtha Paul <nishthapaul@Nishthas-MacBook-Air.local>
2024-04-17 16:23:29 +0530

    Add api to save prescription for a patient

---
+++ Microservices/health-service/src/main/java/com/atyanidan/controller/PrescriptionResponseController.java
@@ -0,0 +1,35 @@
+package com.atyanidan.controller;
+
+import com.atyanidan.entity.PrescriptionResponse;
+import com.atyanidan.entity.elasticsearch.OlapPrescription;
+import com.atyanidan.service.PrescriptionResponseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/prescription-responses")
+public class PrescriptionResponseController {
+
+    private final PrescriptionResponseService prescriptionResponseService;
+
+    @Autowired
+    public PrescriptionResponseController(PrescriptionResponseService prescriptionResponseService) {
+        this.prescriptionResponseService = prescriptionResponseService;
+    }
+
+    @PostMapping
+    public ResponseEntity<PrescriptionResponse> addPrescription(@RequestBody OlapPrescription olapPrescription) {
+        PrescriptionResponse savedPrescriptionResponse = prescriptionResponseService.createPrescriptionResponse(olapPrescription);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedPrescriptionResponse);
+    }
+
+//    @GetMapping("/patient/{patientNumber}")
+//    public List<FormNameTimestampResponse> getFormResponsesByPatientNumber(@PathVariable String patientNumber) {
+//        return formResponseService.getFormsNameAndTimestampByPatientNumber(patientNumber);
+//    }
+}
