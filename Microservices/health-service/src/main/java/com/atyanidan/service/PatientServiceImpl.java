d0e5289c4867288252589fa918d2ee35c8ef2496
Nishtha Paul <nishthapaul@Nishthas-MacBook-Air.local>
2024-04-22 09:59:01 +0530

    Updated list of responses by distinct patient number

--- Microservices/health-service/src/main/java/com/atyanidan/service/PatientServiceImpl.java
+++ Microservices/health-service/src/main/java/com/atyanidan/service/PatientServiceImpl.java
@@ -8,14 +8,12 @@ import com.atyanidan.entity.actor.Doctor;
 import com.atyanidan.entity.elasticsearch.OlapForm;
 import com.atyanidan.exception.NotFoundException;
 import com.atyanidan.model.PatientDataResponse;
+import com.atyanidan.model.PatientDemographicDetailsResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Service
@@ -82,6 +80,9 @@ public class PatientServiceImpl implements PatientService {
             throw new NotFoundException("Doctor doesn't exist");
         }
         List<PrescriptionResponse> prescriptionResponses = prescriptionResponseRepository.findByDoctor(doctor, Sort.by(Sort.Direction.DESC, "submittedOn"));
+
+        prescriptionResponses = getDistinctPatientData(prescriptionResponses);
+
         for (PrescriptionResponse prescriptionResponse : prescriptionResponses) {
             PatientDataResponse response = new PatientDataResponse();
             response.setPatientNumber(prescriptionResponse.getPatient().getPatientNumber());
@@ -97,6 +98,31 @@ public class PatientServiceImpl implements PatientService {
         }
 
         return patientDataResponses;
+    }
+
+    @Override
+    public PatientDemographicDetailsResponse getPatientDemographicDetails(String patientNumber) {
+        Patient patient = findByPatientNumber(patientNumber);
+        return new PatientDemographicDetailsResponse(patientNumber, patient.getDemographic());
+    }
+
+    private List<PrescriptionResponse> getDistinctPatientData(List<PrescriptionResponse> prescriptionResponses) {
+        Map<Patient, PrescriptionResponse> map = new HashMap<>();
+        for (PrescriptionResponse response : prescriptionResponses) {
+            Patient patient = response.getPatient();
+            if ( !map.containsKey(patient) ) {
+                map.put(patient, response);
+            }
+        }
+        List<PrescriptionResponse> responsesListWithoutDuplicates = new ArrayList<>();
+        for (Patient patient : map.keySet()) {
+            responsesListWithoutDuplicates.add(map.get(patient));
+        }
+        responsesListWithoutDuplicates = responsesListWithoutDuplicates.stream()
+                .sorted(Comparator.comparing(PrescriptionResponse::getSubmittedOn).reversed())
+                .collect(Collectors.toList());
+        return responsesListWithoutDuplicates;
+
     }
 
 }
