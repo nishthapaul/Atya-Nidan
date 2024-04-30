6450caffc08b238ef2b7f57693f4b29e792c2b5f
nisharathod231 <nisharathod27899@gmail.com>
2024-04-30 16:57:15 +0530

    Add sqlite database for demographics and forms table

--- FrontEnd/Database/database.js
+++ FrontEnd/Database/database.js
@@ -3,33 +3,43 @@ import * as SQLite from "expo-sqlite";
 export const db = SQLite.openDatabase("app.db");
 
 export const init = () => {
-  const promise = new Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {
     db.transaction((tx) => {
-      tx.executeSql(
-        `DROP TABLE IF EXISTS field_worker;`,
-        [],
-        () => {
-          resolve();
-        },
-        (_, err) => {
-          reject(err);
-        }
-      );
+      tx.executeSql('DROP TABLE IF EXISTS field_worker;', [], null, (_, err) => reject(err));
       tx.executeSql(
         `CREATE TABLE IF NOT EXISTS field_worker (
           empId TEXT PRIMARY KEY NOT NULL,
           talukaId INT NOT NULL,
           districtId INT NOT NULL
-        );`,
-        [],
-        () => {
-          resolve();
-        },
-        (_, err) => {
-          reject(err);
-        }
+        );`, [], null, (_, err) => reject(err)
+      );
+
+      tx.executeSql('DROP TABLE IF EXISTS demographics;', [], null, (_, err) => reject(err));
+      tx.executeSql(
+        `CREATE TABLE IF NOT EXISTS demographics (
+          patientNumber TEXT PRIMARY KEY NOT NULL,
+          firstName TEXT NOT NULL,
+          address TEXT NOT NULL,
+          dob DATE NOT NULL,
+          gender TEXT NOT NULL,
+          bloodGroup TEXT NOT NULL,
+          talukaid INT NOT NULL,
+          phonenumber TEXT NOT NULL
+        );`, [], null, (_, err) => reject(err)
+      );
+
+      tx.executeSql('DROP TABLE IF EXISTS forms;', [], null, (_, err) => reject(err));
+      tx.executeSql(
+        `CREATE TABLE IF NOT EXISTS forms (
+          title TEXT PRIMARY KEY NOT NULL,
+          formId INT NOT NULL,
+          selected INT NOT NULL,
+          formDefinition TEXT NOT NULL,
+          specialisationId INT NOT NULL
+        );`, [], 
+        () => resolve(), // Resolve after all tables are assured to be created
+        (_, err) => reject(err)
       );
     });
   });
-  return promise;
 };
