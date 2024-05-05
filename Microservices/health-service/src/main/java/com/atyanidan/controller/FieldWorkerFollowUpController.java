4e3f782c971f56131edef28962a78d1b462798b7
anarghya15 <anarghya.gayathri@gmail.com>
2024-05-05 15:09:38 +0530

    Add code for Swagger documentation

--- Microservices/health-service/src/main/java/com/atyanidan/controller/FieldWorkerFollowUpController.java
+++ Microservices/health-service/src/main/java/com/atyanidan/controller/FieldWorkerFollowUpController.java
@@ -2,6 +2,13 @@ package com.atyanidan.controller;
 
 import com.atyanidan.entity.FieldWorkerFollowUp;
 import com.atyanidan.service.PrescriptionResponseService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +18,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
+@Tag(name = "Health Service", description = "APIs part of the Health service")
 public class FieldWorkerFollowUpController {
     private final PrescriptionResponseService prescriptionResponseService;
 
@@ -18,6 +26,13 @@ public class FieldWorkerFollowUpController {
         this.prescriptionResponseService = prescriptionResponseService;
     }
 
+    @Operation(summary = "Retrieve list of fieldworkers", description = "Retrieve the list of all the fieldworkers of a given taluka")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = { @Content(mediaType = "application/json",
+                    array = @ArraySchema(schema = @Schema(implementation = FieldWorkerFollowUp.class))) }),
+            @ApiResponse(responseCode = "404", description = "No fieldworkers found",
+                    content = @Content)
+    })
     @GetMapping("/fieldworkers/{fieldworkerId}/followups")
     public List<FieldWorkerFollowUp> getFollowUpsByFieldWorker(@PathVariable String fieldworkerId) {
         return prescriptionResponseService.getFollowUpsOfFieldWorker(fieldworkerId);
--- Microservices/health-service/src/main/java/com/atyanidan/controller/StatsController.java
+++ Microservices/health-service/src/main/java/com/atyanidan/controller/StatsController.java
@@ -1,7 +1,16 @@
 package com.atyanidan.controller;
 
+import com.atyanidan.entity.actor.FieldWorker;
 import com.atyanidan.service.StatsService;
 import com.atyanidan.utils.UnHealthyCountByDate;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +20,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/stats")
+@Tag(name = "Health Service", description = "APIs part of the Health service")
 public class StatsController {
 
     private final StatsService statsService;
@@ -20,8 +30,17 @@ public class StatsController {
         this.statsService = statsService;
     }
 
+    @Operation(summary = "Retrieve stats of healthy patients", description = "Retrieve the count of Unhealthy patients")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = { @Content(mediaType = "application/json",
+                    array = @ArraySchema(schema = @Schema(implementation = UnHealthyCountByDate.class))) }),
+            @ApiResponse(responseCode = "404", description = "No fieldworkers found",
+                    content = @Content)
+    })
     @GetMapping("/admin/{districtId}")
-    public ResponseEntity<List<UnHealthyCountByDate>> getHealthCount(@PathVariable int districtId) {
+    public ResponseEntity<List<UnHealthyCountByDate>> getHealthCount(
+            @Parameter(name = "districtId", description = "District ID", required = true)
+            @PathVariable int districtId) {
         List<UnHealthyCountByDate> healthCount = statsService.getHealthCountByDate(districtId);
         return ResponseEntity.status(HttpStatus.OK).body(healthCount);
     }
